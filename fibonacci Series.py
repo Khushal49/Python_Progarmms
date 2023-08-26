79227dd3e4923cddd0f88a34887fb19015462619
KHUSHAL DHARAT <115824268+Khushal49@users.noreply.github.com>
2023-08-26 16:18:20 +0530

    Add files via upload

---
+++ fibonacci Series.py
@@ -0,0 +1,28 @@
+#fibonacci series
+# n=int(input("enter the number of your choice:"))
+# n1,n2=0,1
+# count=0
+# if n<0:
+#     print("enter the valid positive number:")
+# elif n==1:
+#     print("fibonacci series upto nth term:")
+#     print(n1)
+# else:
+#  while count<n:
+#   print("fibonacci sequence upto nth term:")
+#   print(n1)
+#   nth=n1+n2
+#   n1=n2
+#   n2=nth
+#   count+=1
+  
+#palindrome
+# def palindrome(string):
+#     s1=string
+#     s2=string[::-1]
+#     if s1==s2:
+#         return True
+#     else:
+#         return False
+# print(palindrome("madam"))  
+
